cbf1f221716c6887c9bf242a043d5fc2da4a9a23
Sopan Sharma <sopan.sharma27@gmail.com>
2015-04-14 23:46:07 -0700

    Initial Commit

---
+++ SSBoxCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSBoxCollectionView
+//
+//  Created by Sopan Sharma on 4/14/15.
+//  Copyright (c) 2015 Sopan Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSBoxCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSBoxCollectionView
+//
+//  Created by Sopan Sharma on 4/14/15.
+//  Copyright (c) 2015 Sopan Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
